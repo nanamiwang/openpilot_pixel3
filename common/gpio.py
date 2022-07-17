eff4402f47aef23648df845f501a17bd086fb5f3
Alex Wang <36569033@qq.com>
2022-07-17 17:07:24 +0800

    Disable gpio.py

--- common/gpio.py
+++ common/gpio.py
@@ -1,4 +1,9 @@
+import os
+PIXEL3 = os.path.isfile('/data/pixel3')
+
 def gpio_init(pin: int, output: bool) -> None:
+  if PIXEL3:
+    return
   try:
     with open(f"/sys/class/gpio/gpio{pin}/direction", 'wb') as f:
       f.write(b"out" if output else b"in")
@@ -7,6 +12,8 @@ def gpio_init(pin: int, output: bool) -> None:
 
 
 def gpio_set(pin: int, high: bool) -> None:
+  if PIXEL3:
+    return
   try:
     with open(f"/sys/class/gpio/gpio{pin}/value", 'wb') as f:
       f.write(b"1" if high else b"0")
